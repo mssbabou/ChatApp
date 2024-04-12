8bbbf41a25bc56ade16b6222613ee407cd74e701
DESKTOP-IQEN4JP\Niller <niclassylvest@gmail.com>
2024-04-12 12:03:31 +0200

    Update on layout and functions

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -3,18 +3,35 @@ const connection = new signalR.HubConnectionBuilder()
 .build();
 
 document.getElementById("send-button").onclick = function() {
-    const message = document.getElementById("message-input").value;
-
+    if (document.getElementById("message-input").value.trim() != "")
+    {
+        const message = document.getElementById("message-input").value;
+    document.getElementById("message-input").value = "";
     connection.invoke("SendMessage", "User", message).catch(function(err) {
         return console.error(err.toString());
     });
+    }
+    
 };
 
-connection.on("ReceiveMessage", function (user, message) {
+document.getElementById("message-input").addEventListener("keypress",function(event){
+    document.getElementById("character-input").textContent = document.getElementById("message-input").value.length+1 + "/500";
+    if (event.key === 'Enter' && document.getElementById("message-input").value.trim() != "")
+    {
+        event.preventDefault();
+        document.getElementById("send-button").click();
+        document.getElementById("message-input").value = "";
+    }
+});
+
+
+
+
+connection.on("ReceiveMessage", function (chatMessage) {
 
     var newMessage = document.createElement("div");
-    newMessage.className = user == connection.connectionId ? "message sent" : "message received";
-    newMessage.textContent = message;
+    newMessage.className = chatMessage.user == connection.connectionId ? "message sent" : "message received";
+    newMessage.textContent = chatMessage.message;
 
     document.getElementById("message-list").appendChild(newMessage);
 
