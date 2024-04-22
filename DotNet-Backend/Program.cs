1da79491a97ed3ad4ed80d9ed4a03ca57088fe4b
mssbabou <mssbabou@gmail.com>
2024-04-22 19:06:02 +0200

    Fix Folder Name

---
+++ DotNet-Backend/Program.cs
@@ -0,0 +1,99 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IApiKeyService, ApiKeyService>();
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = "ApiKey";
+    options.DefaultChallengeScheme = "ApiKey";
+}).AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>("ApiKey", null);
+
+builder.Services.AddScoped<MongoDbContext>();
+builder.Services.AddScoped<ChatDatabaseService>();
+builder.Services.AddScoped<NameGenerator>();
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+// Register the Swagger generator
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChatApp API", Version = "v1" });
+
+    // Define the BearerAuth scheme
+    c.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "Input your Bearer token in this format - Bearer {your token here}"
+    });
+
+    // Apply the BearerAuth scheme globally to all operations
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "BearerAuth"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Now that the WebApplication instance is created, configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    ServeUnknownFileTypes = true,
+    DefaultContentType = "application/octet-stream"
+});
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChatApp Api V1");
+});
+
+// Map the SignalR Hub
+app.MapHub<ChatHub>("/chathub");
+
+// Map the REST API
+app.MapControllers();
+
+// Test MongoDB connection on separate thread
+Task testMongoDBConnection = Task.Run(async() =>
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<MongoDbContext>();
+    await dbContext.TestConnectionAsync();
+});
+
+var scope = app.Services.CreateScope();
+var chatDatabaseService = scope.ServiceProvider.GetRequiredService<ChatDatabaseService>();
+
+app.Run();
